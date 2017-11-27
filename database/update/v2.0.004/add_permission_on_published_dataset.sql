f7b5c51a8cfc57ff204a5d25f2eee5c9c30cee30
amouget <anna.mouget@ign.fr>
2017-11-27 18:27:40 +0100

    #1127: Add public jdd pages - fix database scripts

--- database/update/v2.0.004/add_permission_on_published_dataset.sql
+++ database/update/v2.0.004/add_permission_on_published_dataset.sql
@@ -1,6 +1,6 @@
-INSERT INTO website.permisssions(permission_code, permission_label) VALUES ('VIEW_PUBLISHED_DATASETS','Visualiser les jeux de données ayant fait l''objet d''un dépôt légal');
+INSERT INTO website.permission(permission_code, permission_label) VALUES ('VIEW_PUBLISHED_DATASETS','Visualiser les jeux de données ayant fait l''objet d''un dépôt légal');
 
 INSERT INTO website.permission_per_role(role_code, permission_code) 
  (SELECT role_code, 'VIEW_PUBLISHED_DATASETS'
     FROM website.permission_per_role 
-   GROUP BY role_code)
+   GROUP BY role_code);
