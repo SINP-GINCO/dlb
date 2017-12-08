c925402d5a5866de4347900d9a7012b6525d9240
Gautam <gautam.pastakia@ign.fr>
2017-12-08 11:28:30 +0100

    #1114: Organize imports

--- website/server/src/Ign/Bundle/DlbBundle/Controller/DBBController.php
+++ website/server/src/Ign/Bundle/DlbBundle/Controller/DBBController.php
@@ -1,17 +1,17 @@
 <?php
 namespace Ign\Bundle\DlbBundle\Controller;
 
-use Ign\Bundle\OGAMBundle\Controller\GincoController;
 use Ign\Bundle\GincoBundle\Entity\RawData\DEE;
 use Ign\Bundle\GincoBundle\Entity\Website\Message;
 use Ign\Bundle\GincoBundle\Exception\DEEException;
+use Ign\Bundle\OGAMBundle\Controller\GincoController;
 use Ign\Bundle\OGAMBundle\Entity\RawData\Jdd;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 /**
