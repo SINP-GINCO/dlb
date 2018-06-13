0d4717e20762957452c635f31647fcb099ba0496
vsagniez <vincent.sagniez@ign.fr>
2018-06-13 09:10:42 +0200

    #1531: Correct Error script

--- database/update/v2.2.000/update_permission.sql
+++ database/update/v2.2.000/update_permission.sql
@@ -1,9 +1,4 @@
 INSERT INTO permission_per_role
-  SELECT role_code, 'MANAGE_OWNED_PRIVATE_REQUEST'
-  FROM website.role
-  WHERE role_label = 'Administrateur';
-  
-INSERT INTO permission_per_role
   SELECT role_code, 'GENERATE_DEE_OWN_JDD'
   FROM website.role
   WHERE role_label = 'Administrateur';
