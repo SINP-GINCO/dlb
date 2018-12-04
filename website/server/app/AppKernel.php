b1cbabf38e363538dd3a9b1536c7ca7fdc0e7a1d
Rémi Pas <remi.pas@ign.fr>
2018-12-04 16:27:42 +0100

    new symfony structure

--- website/server/app/AppKernel.php
+++ website/server/app/AppKernel.php
@@ -40,4 +40,12 @@ class AppKernel extends Kernel {
 	public function registerContainerConfiguration(LoaderInterface $loader) {
 		$loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
 	}
+	
+	public function getCacheDir() {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+	
+    public function getLogDir() {
+        return dirname(__DIR__).'/var/logs';
+    }
 }
