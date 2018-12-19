919cbe9ce898fc06dd73858990048caa119582ba
vsagniez <vincent.sagniez@ign.fr>
2018-12-19 15:57:18 +0100

    #1563: Calcul nom Valide

--- database/update/v2.3.000/update_dlb.php
+++ database/update/v2.3.000/update_dlb.php
@@ -49,6 +49,11 @@ $CLIParams = implode(' ', array_slice($argv, 1));
 try {
 
 	//system("php $sprintDir/XXXX.php $CLIParams", $returnCode1);
+    system("php $sprintDir/update_model_nomvalide.php $CLIParams", $returnCode1) ;
+    if ($returnCode1 != 0) {
+        echo "$sprintDir/update_model_nomvalide.php crashed.\n";
+        exit(1);
+    }
 	
 
 } catch (Exception $e) {
