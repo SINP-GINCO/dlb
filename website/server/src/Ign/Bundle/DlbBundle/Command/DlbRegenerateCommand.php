b1c562eda659c9821cb6b6447f900a30afea2d8f
Rémi Pas <remi.pas@ign.fr>
2020-01-22 11:23:23 +0100

    [#1808] refactor dlb:generate to make it more usable

--- website/server/src/Ign/Bundle/DlbBundle/Command/DlbRegenerateCommand.php
+++ website/server/src/Ign/Bundle/DlbBundle/Command/DlbRegenerateCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -22,7 +23,7 @@ use Ign\Bundle\GincoBundle\Entity\RawData\Submission;
 class DlbRegenerateCommand extends Command {
 	
 	
-	protected static $defaultName = 'dlb:regenerate:dbb' ;
+	protected static $defaultName = 'dlb:regenerate' ;
 
 
 	/**
@@ -56,8 +57,8 @@ class DlbRegenerateCommand extends Command {
 	public function configure() {
 		$this
 			->setDescription('Régénère les fichiers de données brutes de biodiversité.')
-			->addOption('start', null, InputOption::VALUE_REQUIRED, 'Date de début au format YYYY-MM-DD.')
-			->addOption('end', null, InputOption::VALUE_REQUIRED, 'Date de fin au format YYYY-MM-DD.')
+			->addOption('jddId', 'j', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, "Identifiant métadonnée du JDD à régénérer")
+            ->addOption('file', 'f', InputOption::VALUE_OPTIONAL, "Fichier contenant une liste d'identifiants de métadonnées de JDD à régénérer")
 		;
 	}
 	
@@ -70,93 +71,73 @@ class DlbRegenerateCommand extends Command {
 	 */
 	public function execute(InputInterface $input, OutputInterface $output) {
 		
-		$start = null ;
-		$end = null ;
-		
-		// Parse start date.
-		if ($input->hasOption('start')) {
-			$start = \DateTime::createFromFormat('Y-m-d', $input->getOption('start')) ;
-			if (!$start) {
-				$start = null ;
-			}
-		}
-		
-		// Parse end date.
-		if ($input->hasOption('end')) {
-			$end = \DateTime::createFromFormat('Y-m-d', $input->getOption('end')) ;
-			if (!$end) {
-				$end = null ;
-			}
-		}
-		
-		// Si aucune date fournie, on ne fait rien.
-		if (!$start && !$end) {
-			$output->writeln("<error>Aucune date de début ou de fin fournie.</error>") ;
+        if (!$input->hasOption('jddId') && !$input->hasOption('file')) {
+            $output->writeln("<error>Aucun identifiant de jeu de données n'a été fourni.</error>") ;
 			return 1 ;
-		}
+        }
+        
+        $jddIds = array() ;
+        if ($input->hasOption('jddId')) {
+            $jddIds = $input->getOption('jddId') ;
+        }
+        
+        if ($input->hasOption('file')) {
+            $filepath = $input->getOption('file') ;
+            if (!empty($filepath)) {
+                $file = new \SplFileObject($filepath, "r") ;
+                $jddIds = array_merge($jddIds, $this->readJddIdFromFile($file)) ;
+            }
+        }
 		
-		$dees = $this->findPublishedDees($start, $end) ;
-		$numDees = count($dees) ;
-		
-		$output->writeln("") ;
-		$output->writeln("<info>Nombre de dépots régénérés : $numDees</info>") ;
-		$output->writeln("<info>Date de début : " . $input->getOption('start') . "</info>") ;
-		$output->writeln("<info>Date de fin : " . $input->getOption('end') . "</info>") ;
-		
-		foreach ($dees as $dee) {
-			// Régénération du dépot, sans notification de l'utilisateur.
-			$output->writeln("") ;
-			$output->writeln("Régénération DBB {$dee->getId()}, pour le JDD {$dee->getJdd()->getId()}.") ;
+        $jddRepository = $this->entityManager->getRepository('IgnGincoBundle:RawData\Jdd') ;
+        $deeRepository = $this->entityManager->getRepository('IgnGincoBundle:RawData\DEE') ;
+        
+        foreach ($jddIds as $jddId) {
             
-            $files = $this->dbbProcess->getDbbGenerator($dee)->generate($dee) ;
-            // Create archive and delete useless csv file.
-			$this->dbbProcess->createDBBArchive($dee, $files) ;
-			foreach ($files as $file) {
-				@unlink($file);
-			}
-                        
-			$output->writeln("Terminé") ;
-		}
+            $output->writeln("<info>### Régénération du Jdd $jddId :</info>") ;
+            $output->writeln("");
+            
+            $jdd = $jddRepository->findOneByMetadataId($jddId) ;
+            
+            if (!$jdd) {
+                $output->writeln("<error>Jdd $jddId non trouvé.</error>") ;
+                continue ;
+            }
+            
+            if (!$jdd->isActive()) {
+                $output->writeln("<info>Jdd $jddId a été supprimé.</info>") ;
+                continue ;
+            }
+            
+            $dee = $deeRepository->findLastVersionByJdd($jdd) ;
+            if (!$dee) {
+                $output->writeln("<info>Pas de DEE trouvée pour le Jdd $jddId.</info>") ;
+                continue ;
+            }
+            
+            $this->dbbProcess->unfreezeJdd($jdd) ;
+            $this->dbbProcess->generateAndSendDBB($dee, false) ;
+        }
+
 	}
-	
-	
-	
-	/**
-	 * Trouve les DEEs qui ont été publiés.
-	 * @param \DateTime $start
-	 * @param \DateTime $end
-	 * @return DEE[]
-	 */
-	private function findPublishedDees($start, $end) {
-		
-		$deeRepository = $this->entityManager->getRepository('IgnGincoBundle:RawData\DEE') ;
-		$queryBuilder = $deeRepository->createQueryBuilder('d')
-			->leftJoin('d.jdd', 'j')
-			->leftJoin('j.fields', 'f')
-			->where('d.status = :deeStatus')
-			->andWhere('j.status = :jddStatus')
-			->andWhere('f.key = :key')
-			->andWhere('f.valueString = :value')
-			->setParameter('deeStatus', DEE::STATUS_OK)
-			->setParameter('jddStatus', Jdd::STATUS_ACTIVE)
-			->setParameter('key', 'status')
-			->setParameter('value', 'published')
-		;
-		
-		if ($start) {
-			$queryBuilder
-				->andWhere('d.createdAt >= :start')
-				->setParameter('start', $start)
-			;
-		}
-		
-		if ($end) {
-			$queryBuilder
-				->andWhere('d.createdAt < :end')
-				->setParameter('end', $end)
-			;
-		}
-		
-		return $queryBuilder->getQuery()->getResult() ;
-	}
+    
+    
+    /**
+     * Lit les identifiants contenus dans un fichier et les renvoie sous forme de tableau.
+     * @param \SplFileObject $file
+     * @return array
+     */
+    private function readJddIdFromFile(\SplFileObject $file) {
+         
+        $jddIds = array() ;
+        while (!$file->eof()) {
+            $row = trim($file->fgets()) ;
+            if (empty($row)) {
+                continue ;
+            }
+           $jddIds[] = $row ; 
+        }
+         
+        return $jddIds ;
+    }
 }
